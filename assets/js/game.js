4aea2bf4a987dcfa2a3015b76b28cb98e593033e
Lucas P1 <piwoskape@gmail.com>
2022-04-27 10:52:36 +0000

    js basic functions structure added

---
+++ assets/js/game.js
@@ -0,0 +1,18 @@
+
+// function for starting game
+function startGame(){
+
+
+}
+
+// function displaying next question and answers
+function showNextQuestion(nextQuestionIndex){
+
+}
+
+// event listener for which option was chosed 
+function selectOption(option) {
+
+}
+
+startGame()
