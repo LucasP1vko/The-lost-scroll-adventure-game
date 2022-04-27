24dec7663e6978899aef31ad451c97c3d7baded0
Lucas P1 <piwoskape@gmail.com>
2022-04-27 20:39:46 +0000

    question area style added to centre text

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,122 +1,114 @@
-const storyQuestionElement = document.getElementById('story-question-text')
-const optionButtonsElements = document.getElementById('options-buttons')
+let storyQuestionElement = document.getElementById('story-question-text');
+// let optionButtonsElements = document.getElementById('options-buttons');
 
-// counts how many lifes player have
-const lifeCounter = {}
 
-// function for starting game
 function startGame() {
-    lifeCounter = {}
-    showNextQuestion(0)
-
+showNextQuestion(0);
 }
 
 // function displaying next question and answers
-function showNextQuestion(nextQuestionIndex) {
-
+function showNextQuestion(nextQuestion) {
+let question = questionIndex.find(question => question.id === nextQuestion)
+storyQuestionElement.innerHTML = question.text
 }
 
 // event listener for which option was chosed 
-function selectOption(option) {
+// function selectOption(option) {
 
-}
+// }
+let exampleText = '<h1>The Lost Scroll.</h1>' +
+'<h4>Text adventure game.</h4>';
 
-// list of questions and answers options called with id
-const questionIndex = [
-    // game intro screen
-    {
-        id: 0,
-        text: '<h1>The Lost Scroll.</h1>' +
-            '<h4>Text adventure game.</h4>',
-        // need to implement input here to ask for user name
-        options: [{
-            text: 'Begin quest',
-            lifeCounter = {
-                3
-            },
-            nextText: 1
-        }, ]
-    },
-    // end game screen
-    {
-        id: -1,
-        // this need to be fixed
-        lifeCounter = {
-            lifeCounter - 1
-        },
-        text: '<h1>You died.</h1>'
-        options: [{
-            text: 'Try again. Lives left: ${lifeCounter}',
+//  list of questions and answers options called with id
+ const questionIndex = [ 
+     // game intro screen
+     {
+         id: 0,
+         text: '<h1>The Lost Scroll.</h1>' +
+             '<h4>Text adventure game.</h4>',
+         // need to implement input here to ask for user name
+         options: [{
+             text: 'Begin quest',
 
-            nextText: 1
-        }, ]
-    },
-    // first question
-    {
-        id: 1,
-        text: 'One night you woke up alone on cold rocky beach and you dont know where you are and how you got here.' +
-            'The only thing you remember is that you were with your family on a ship to england and you went to sleep in your cabin.' +
-            'The ship must have sunked when you were sleeping' +
-            'You need to find your family' +
-            'Its dark but you look around where to go:'
-        options: [{
-                text: 'Tall wall of rocks on front (climb)',
+             nextText: 1
+         }, ]
+     },
+// end game screen
+     {
+         id: -1,
 
-                nextText: 'endGame',
-                alert(`You have dropped off the wall and died.`)
-            },
-            {
-                text: 'Complete darkness on your left (go left)',
+         text: '<h1>You died.</h1>',
+         options: [{
+             text: 'Try again. Lives left: ${lifeCounter}',
+             nextText: 1
+         }, ]
+     },
+     // first question
+     {
+         id: 1,
+         text: 'One night you woke up alone on cold rocky beach and you dont know where you are and how you got here.' +
+             'The only thing you remember is that you were with your family on a ship to england and you went to sleep in your cabin.' +
+             'The ship must have sunked when you were sleeping' +
+             'You need to find your family' +
+             'Its dark but you look around where to go:',
+         options: [{
+                 text: 'Tall wall of rocks on front (climb)',
 
-                nextText: 'endGame',
-                alert(`Sea tide flooded the beach and you drowned.`)
-            },
-            {
-                text: 'Barelly visible light in the distance on your right (go right)',
-                nextText: 2,
-            },
-            {
-                text: 'Stay here and wait for the day',
+                 nextText: -1,
+                //  alert(`You have dropped off the wall and died.`)
+             },
+             {
+                 text: 'Complete darkness on your left (go left)',
 
-                nextText: 'endGame',
-                alert(`Sea tide flooded the beach and you drowned.`)
-            },
-        ]
-    },
+                 nextText: -1,
+                //  alert(`Sea tide flooded the beach and you drowned.`)
+             },
+             {
+                 text: 'Barelly visible light in the distance on your right (go right)',
+                 nextText: 2,
+             },
+             {
+                 text: 'Stay here and wait for the day',
 
-    // second question
+                 nextText: -1,
+                //  alert(`Sea tide flooded the beach and you drowned.`)
+             },
+         ]
+     },
 
-    {
-        id: 2,
-        text: 'After long walk with last of your strenghts you arrived to the source of light.' +
-            'It appeared to be small camping campside on front of entrance to the cave covered with massive rock with some ancient mechanizm to move it.' +
-            'Beside fireplace was an old man with his dog. He look like he is a guardian of this entrance.' +
-            'what do you do?',
-        options: [{
-                text: 'Wait for daytime',
+     // second question
 
-                nextText: 'endGame',
-                alert(`Sea tide flooded the beach and you drowned.`)
-            },
-            {
-                text: 'Call him',
-                nextText: 3,
-            },
-            {
-                text: 'Walk to him',
+     {
+         id: 2,
+         text: 'After long walk with last of your strenghts you arrived to the source of light.' +
+             'It appeared to be small camping campside on front of entrance to the cave covered with massive rock with some ancient mechanizm to move it.' +
+             'Beside fireplace was an old man with his dog. He look like he is a guardian of this entrance.' +
+             'what do you do?',
+         options: [{
+                 text: 'Wait for daytime',
 
-                nextText: -1,
-                alert(`The guard shot you without warning.`)
+                 nextText: -1,
+                //  alert(`Sea tide flooded the beach and you drowned.`)
+             },
+             {
+                 text: 'Call him',
+                 nextText: 3,
+             },
+             {
+                 text: 'Walk to him',
 
-            },
-            {
-                text: 'Sneak to the camp.',
+                 nextText: -1,
+                //  alert(`The guard shot you without warning.`)
 
-                nextText: -1,
-                alert(`Guards dog sniffs you and you got shot.`)
-            },
-        ]
-    }
-]
+             },
+             {
+                 text: 'Sneak to the camp.',
+
+                 nextText: -1,
+                //  alert(`Guards dog sniffs you and you got shot.`)
+             },
+         ]
+     }
+ ]
 
 startGame()
